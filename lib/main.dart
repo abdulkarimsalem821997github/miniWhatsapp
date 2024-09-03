065f028038a90c06f1489cba2f23299b3810a824
abdulkarim <abdulkarimsalem1997@gmail.com>
2024-09-03 11:11:31 +0300

    first commet

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'widgets/welcome_screen_widget.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Welcome Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
