c041bee35b9c19a1ed8f071146024e21bbc2851e
abdulkarim <abdulkarimsalem1997@gmail.com>
2024-09-03 11:41:11 +0300

    edit pating text

--- lib/widgets/welcome_screen_widget.dart
+++ lib/widgets/welcome_screen_widget.dart
@@ -8,65 +8,63 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFF111B21), // Dark background color
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const SizedBox(height: 20),
-                const Text(
-                  'Welcome to WhatsApp',
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 30),
+          child: Column(
+          
+            children: <Widget>[
+              const SizedBox(height: 20),
+              const Text(
+                'Welcome to WhatsApp',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              // const SizedBox(height: 20),
+              const Expanded(
+                child:  CircleAvatar(
+                  radius: 100,
+                  backgroundColor: Color(0xFF25D366), // Green WhatsApp color
+                  backgroundImage:
+                      AssetImage("assets/images/welcome-whatsapp.jpeg"),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor:
+                      const Color(0xFF008364), // Green WhatsApp color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'AGREE AND CONTINUE',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 24,
+                    fontSize: 16,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                // const SizedBox(height: 20),
-                const Expanded(
-                  child:  CircleAvatar(
-                    radius: 100,
-                    backgroundColor: Color(0xFF25D366), // Green WhatsApp color
-                    backgroundImage:
-                        AssetImage("assets/images/welcome-whatsapp.jpeg"),
-                  ),
-                ),
-                const SizedBox(height: 20),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 40.0),
-                  child: Text(
-                    'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white70,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 40),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        const Color(0xFF008364), // Green WhatsApp color
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'AGREE AND CONTINUE',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
